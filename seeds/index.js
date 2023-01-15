4a264a1ee0d0d4a5b748102c0e94e466e0bc6cc9
Anton Krasnikov <krasnikovanton84@gmail.com>
2023-01-15 14:23:29 -0800

    Updates seeds

--- seeds/index.js
+++ seeds/index.js
@@ -6,16 +6,16 @@ const seedPosts = require('./posts-seeds');
 const sequelize = require('../config/connection');
 
 const seedAll = async () => {
-  await sequelize.sync({ force: true });
-  console.log('\n----- DATABASE SYNCED -----\n');
-  await seedUsers();
-  console.log('\n----- USERS SEEDED -----\n');
+  // await sequelize.sync({ force: true });
+  // console.log('\n----- DATABASE SYNCED -----\n');
+  // await seedUsers();
+  // console.log('\n----- USERS SEEDED -----\n');
 
   await seedPosts();
   console.log('\n----- POSTS SEEDED -----\n');
 
-  await seedComments();
-  console.log('\n----- COMMENTS SEEDED -----\n');
+  // await seedComments();
+  // console.log('\n----- COMMENTS SEEDED -----\n');
 
   process.exit(0);
 };
--- seeds/users-seeds.js
+++ seeds/users-seeds.js
@@ -1,16 +1,8 @@
 const { User } = require('../models');
 
 const usersData = [
-    {
-      "username": "kas500",
-      "password": "123456"
-    },
-    {
-        "username": "bueno_user",
-        "password": "1234567"
-      }
   ]
 
-const seedUsers = () => User.bulkCreate(usersData);
+//const seedUsers = () => User.bulkCreate(usersData);
 
-module.exports = seedUsers;
+//module.exports = seedUsers;
