13fabdf94c9bad8f3e5efefec3b95d8e8ac7169a
Anton Krasnikov <krasnikovanton84@gmail.com>
2023-01-15 14:25:02 -0800

    Updates server

--- server.js
+++ server.js
@@ -34,7 +34,6 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static(path.join(__dirname, 'public')));
 
-
 app.use(routes);
 
 sequelize.sync({ force: false }).then(() => {
