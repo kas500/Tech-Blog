2bd6279030245869c6f528d09aff3e51831bc083
Anton Krasnikov <krasnikovanton84@gmail.com>
2023-01-15 14:22:05 -0800

    Adds comment handler

---
+++ public/js/comment.js
@@ -0,0 +1,26 @@
+const postButtons = document.querySelectorAll('#postComment');
+  for (let i = 0; i < postButtons.length; i++) {
+    postButtons[i].addEventListener('click', async function (e) {
+      const commentText = e.target.parentNode.parentNode.querySelector("#commentText").value.trim();
+      const userId = e.target.parentNode.parentNode.querySelector("#commentText").getAttribute("userId");
+      const postId = e.target.parentNode.parentNode.querySelector("#commentText").getAttribute("postId");
+      
+      if (commentText) {
+        const response = await fetch('/api/comments', {
+          method: 'POST',
+          body: JSON.stringify({commentText, userId, postId}),
+          headers: { 'Content-Type': 'application/json' },
+        });
+    
+        if (response.ok) {
+          document.location.replace('/');
+        } else {
+        
+          alert("Cannot post a comment");
+        }
+      }
+      else{
+        alert("Comment cannot be empty");
+      }
+    });
+  }
