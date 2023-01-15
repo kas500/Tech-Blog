9fa2af674700fb77b9c2fe5031d063e3699cc46d
Anton Krasnikov <krasnikovanton84@gmail.com>
2023-01-15 14:17:06 -0800

    Adds homepage and dashboard renders

--- controllers/home-routes.js
+++ controllers/home-routes.js
@@ -1,5 +1,4 @@
 const router = require('express').Router();
-const { registerDecorator } = require('handlebars');
 const { Post, Comment, User } = require('../models');
 //Import the auth middleware
 const withAuth = require('../utils/auth');
@@ -17,19 +16,45 @@ router.get('/', async (req, res) => {
             {
               model: Comment,
               attributes: ['comment','user_id','updatedAt'],
+              order: [['updatedAt', 'ASC']],
               include: [{
                 model: User,
                 attributes: ['username']
-              }]
+              }],
             },
           ],
         });
       const posts = postsData.map((post) => post.get({ plain: true }));
-      res.render('homepage', {posts, loggedIn: req.session.loggedIn, userName: req.session.username});
+      res.render('homepage', {
+        posts, 
+        loggedIn: req.session.loggedIn, 
+        userName: req.session.username,
+        userId: req.session.userId
+      });
     } catch (err) {
       res.status(500).json(err);
     }
   });
+  router.get('/dashboard',withAuth , async (req, res) => {
+    // find all posts with comments
+    try {
+        const postsData =  await Post.findAll({
+          where: {
+              user_id: req.session.userId
+            },
+          });
+        const posts = postsData.map((post) => post.get({ plain: true }));
+        res.render('homepage', {
+          posts, 
+          loggedIn: req.session.loggedIn, 
+          userName: req.session.username,
+          userId: req.session.userId,
+          url: req.url
+        });
+      } catch (err) {
+        res.status(500).json(err);
+      }
+    });
 
   router.get('/login', (req, res) => {
     console.log(req.session.loggedIn);
