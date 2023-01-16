c5cba884d0f4d0c751250e39ffc8ee16d89cd953
Anton Krasnikov <krasnikovanton84@gmail.com>
2023-01-15 19:33:50 -0800

    Adds auth middleware, udate and delete routes

--- controllers/api/dashboard-routes.js
+++ controllers/api/dashboard-routes.js
@@ -1,20 +1,56 @@
 const router = require('express').Router();
 const { response } = require('express');
 const { Post } = require('../../models');
+const withAuth = require('../../utils/auth');
 
 // CREATE new post
-router.post('/', async (req, res) => {
+router.post('/', withAuth, async (req, res) => {
     try {
         const dbPostData = await Post.create({
-            subject: req.body.postSubject,
-            article: postArticle,
+            subject: req.body.subject,
+            article: req.body.article,
             user_id: req.body.user_id
           });
-            console.log(dbPostData);
         res.status(200).json(dbPostData);
     } 
     catch (error) {
         console.log("from dash routes", response.status);
+        res.status(500).json(error);
+    }
+});
+
+// Update post
+router.put('/update',withAuth, async (req, res) => {
+    try {
+        const dbPostUpdatedData = await Post.update(
+            {
+                subject: req.body.subject,
+                article: req.body.article
+            },
+            {
+                where: {id: req.body.id }
+            },
+          );
+        res.status(200).json(dbPostUpdatedData);
+    } 
+    catch (error) {
+        console.log(response.status);
+        res.status(500).json(error);
+    }
+});
+
+// Delete post
+router.delete('/delete', withAuth, async (req, res) => {
+    try {
+        const dbPostDeletedData = await Post.destroy(
+            {
+                where: {id: req.body.id }
+            },
+          );
+        res.status(200).json(dbPostDeletedData);
+    } 
+    catch (error) {
+        console.log(response.status);
         res.status(500).json(error);
     }
 });
